fecd2b74a95148e53aa874c43a21a103f04221f7
CafteriaElDicho <cmjg2225i@gmail.com>
2025-06-10 19:27:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,17 +6,18 @@
 </head>
 <body>
 
-  <button>Obtener descuento</button>
+  <button id="descuentoBtn">Obtener descuento</button>
 
   <script>
     window.onload = function() {
       var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "");
       alert(`¡Es un gusto tenerte aquí, ${reply}!`);
-    }
 
-    let miBoton = document.querySelector('button');
-    miBoton.onclick = function() {
-      alert('Con el código: JT05 obtendrás un 5% en tu próxima compra');
+      // Aseguramos que el botón esté disponible al momento de asignar el evento
+      let miBoton = document.getElementById('descuentoBtn');
+      miBoton.onclick = function() {
+        alert('Con el código: JT05 obtendrás un 5% en tu próxima compra');
+      }
     }
   </script>
 
