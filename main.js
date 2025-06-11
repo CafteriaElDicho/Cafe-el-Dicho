24e8dda233371e538429ea08881b5666f5fad638
CafteriaElDicho <cmjg2225i@gmail.com>
2025-06-10 19:31:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,6 @@
       var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "");
       alert(`¡Es un gusto tenerte aquí, ${reply}!`);
 
-      // Aseguramos que el botón esté disponible al momento de asignar el evento
       let miBoton = document.getElementById('descuentoBtn');
       miBoton.onclick = function() {
         alert('Con el código: JT05 obtendrás un 5% en tu próxima compra');
