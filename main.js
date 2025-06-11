f0dc2cd9609d78d6a7165a456e145e01fdecfa78
CafteriaElDicho <cmjg2225i@gmail.com>
2025-06-10 19:16:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-window.onload = function() {
-var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "")
-alert ( "Es un gusto tenerte aquí + reply + "!")
-}
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Bienvenida</title>
+</head>
+<body>
 
-let miBoton = document.querySelector('button');
-miBoton.onclick = function() {
-alert('Con el código: JT05 obtendrás un 5% en tu próxima compra');
-}
+  <button>Obtener descuento</button>
+
+  <script>
+    window.onload = function() {
+      var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "");
+      alert(`¡Es un gusto tenerte aquí, ${reply}!`);
+    }
+
+    let miBoton = document.querySelector('button');
+    miBoton.onclick = function() {
+      alert('Con el código: JT05 obtendrás un 5% en tu próxima compra');
+    }
+  </script>
+
+</body>
+</html>
